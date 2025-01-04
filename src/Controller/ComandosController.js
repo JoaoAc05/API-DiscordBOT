9f109250b92d7ed65adb359cfb766fe6ac4c3c64
João Alves <joaoalvescordeiro2005@gmail.com>
2025-01-04 11:15:41 -0400

    debug

--- src/Controller/ComandosController.js
+++ src/Controller/ComandosController.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const comandosPath = path.join(process.cwd(), "Data", "Comandos.json");
+const comandosPath = path.join(process.cwd(), 'Data', 'Comandos.json');
 console.log(`Acessando arquivo: ${comandosPath}`);
 
 class ComandosController {
